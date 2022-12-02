4413db152d97647d8dd59555a823984bc0853f24
Sharafaddin Huseynov <114855172+Sharafaddin0@users.noreply.github.com>
2022-12-02 01:37:27 +0100

    Create a static web interface

--- web.py
+++ web.py
@@ -8,3 +8,8 @@ st.title("Simple To-Do app")
 st.subheader("This is my todo app")
 st.write("Increase your productivity")
 
+for todo in list:
+    st.checkbox(todo)
+
+st.text_input(label = " ", placeholder = "Add a new todo...")
+
